1b7b3ce983dbe42bacb4b3761e17384f4c775654
yaswanth63796 <yeshuv24@gmail.com>
2026-02-14 15:57:00 +0530

    file modified

--- src/config/firebase.config.js
+++ src/config/firebase.config.js
@@ -1,18 +1,13 @@
-const admin = require("firebase-admin");
+import admin from "firebase-admin";
 
 if (!process.env.FIREBASE_KEY) {
-  throw new Error(
-    "FIREBASE_KEY environment variable is not set. Add it in Render's Environment Variables."
-  );
+  throw new Error("FIREBASE_KEY environment variable not set!");
 }
 
 const serviceAccount = JSON.parse(process.env.FIREBASE_KEY);
-
-// Fix private key formatting
-serviceAccount.private_key = serviceAccount.private_key.replace(/\\n/g, '\n');
 
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
 });
 
-module.exports = admin;
+export default admin;
